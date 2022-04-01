1973ddcdf8b3b15b1f8daed86f3496138a2a661b
sooros5132 <sooros5132@gmail.com>
2022-04-02 00:44:10 +0900

    1자리 일 경우 소수점 2번째 까지 추가

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,25 +1,25 @@
 export function koPriceLabelFormat(price: number) {
-  if (!price) return "";
+  if (!price) return '';
 
   const units = [
-    "원",
-    "만",
-    "억",
-    "조",
-    "경",
-    "해",
-    "자",
-    "양",
-    "구",
-    "간",
-    "정",
-    "재",
-    "극",
-    "항하사",
-    "아승기",
-    "나유타",
-    "불가사의",
-    "무량수",
+    '원',
+    '만',
+    '억',
+    '조',
+    '경',
+    '해',
+    '자',
+    '양',
+    '구',
+    '간',
+    '정',
+    '재',
+    '극',
+    '항하사',
+    '아승기',
+    '나유타',
+    '불가사의',
+    '무량수'
   ];
 
   let unitIndex = 0;
@@ -30,9 +30,9 @@ export function koPriceLabelFormat(price: number) {
     scaledValue /= 10000;
   }
 
-  return `${Number(scaledValue.toFixed(0)).toLocaleString()}${
-    units[unitIndex]
-  }`;
+  return `${Number(
+    scaledValue.toFixed().length === 1 ? scaledValue.toFixed(2) : scaledValue.toFixed()
+  ).toLocaleString()}${units[unitIndex]}`;
 }
 
 export const krwRegex = /^krw-/i;
