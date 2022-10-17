52dc4c888db1f4d6b5021f390b212b4e11a72a78
SOOROS <sooros5132@gmail.com>
2022-10-17 13:06:11 +0900

    미디어 쿼리 누락 수정

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,12 @@ module.exports = {
   darkMode: ['class'],
   theme: {
     screens: {
-      sm: '600px'
+      sm: '600px',
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      '2xl': '1536px'
     },
     extend: {
       fontFamily: {
