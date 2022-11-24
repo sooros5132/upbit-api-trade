7b857240bf0e0c45bb593df42ae4410905c09eba
SOOROS <sooros5132@gmail.com>
2022-11-24 17:58:14 +0900

    upbit candle type 추가

--- src/types/upbit.ts
+++ src/types/upbit.ts
@@ -150,3 +150,22 @@ export interface IUpbitSocketMessageTickerSimple {
   tms: number;
   st: string;
 }
+
+export interface IUpbitCandle {
+  market: string; // 마켓명
+  candle_date_time_utc: string; // 캔들 기준 시각(UTC 기준)포맷: yyyy-MM-dd'T'HH:mm:ss
+  candle_date_time_kst: string; // 캔들 기준 시각(KST 기준)포맷: yyyy-MM-dd'T'HH:mm:ss
+  opening_price: number; // 시가
+  high_price: number; // 고가
+  low_price: number; // 저가
+  trade_price: number; // 종가
+  timestamp: number; // 마지막 틱이 저장된 시각
+  candle_acc_trade_price: number; // 누적 거래 금액
+  candle_acc_trade_volume: number; // 누적 거래량
+  unit?: number; //분 단위(유닛)
+  prev_closing_price?: number; // 전일 종가(UTC 0시 기준)
+  change_price?: number; // 전일 종가 대비 변화 금액
+  change_rate?: number; // 전일 종가 대비 변화량
+  converted_trade_price?: number; // 종가 환산 화폐 단위로 환산된 가격(요청에 convertingPriceUnit 파라미터 없을 시 해당 필드 포함되지 않음.)
+  first_day_of_period?: string; // 캔들 기간의 가장 첫 날
+}
