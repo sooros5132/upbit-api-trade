38fe72264f9d09119d76f8cbb92232e050bce256
SOOROS <sooros5132@gmail.com>
2022-12-21 23:46:07 +0900

    사이트 최소 사이즈 추가

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -7,7 +7,7 @@ interface LayoutProps {
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
-    <div className='flex flex-col h-full min-h-screen lg:min-h-[800px] lg:h-screen'>
+    <div className='flex flex-col min-w-[320px] h-full min-h-screen lg:min-h-[800px] lg:h-screen'>
       <Header />
       {children}
       <Footer />
