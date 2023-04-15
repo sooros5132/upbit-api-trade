0a24e7715bb2020309c39f5cfc1e829b0ee1ad2a
SOOROS <sooros5132@gmail.com>
2023-04-15 23:08:21 +0900

    Update env.test.ts

--- test/env.test.ts
+++ test/env.test.ts
@@ -1,12 +1,14 @@
+import siteConfig from 'site-config';
+
 it('tradingview charting_library setting', () => {
-  const TV_CHARTING_LIBRARY = process.env.NEXT_PUBLIC_TV_CHARTING_LIBRARY;
-  if (process.env.TV_CHARTING_LIBRARY !== 'true') {
+  const haveTvChartingLibrary = siteConfig.haveTvChartingLibrary;
+  if (!haveTvChartingLibrary) {
     const err =
-      'ERROR - `tradingview/charting_library` 라이브러리가 필요합니다. 자세한 내용은 `.env.sample`을 확인해주세요.';
+      'ERROR - `tradingview/charting_library` 라이브러리 세팅이 필요합니다. 자세한 내용은 `site-config.ts`파일을 확인해주세요.';
     console.log(err);
   }
 
-  expect(TV_CHARTING_LIBRARY).toBe('true');
+  expect(haveTvChartingLibrary).toBe(true);
 });
 
 export {};
